2441157879d45fc77ef71d12904a165ace19f1f3
Karen <karen@weareike.com>
2024-04-03 15:40:50 -0400

    Finished pet filtering

--- main.js
+++ main.js
@@ -19,6 +19,7 @@ async function petsArea() {
   const petsData = await petsPromise.json()
   petsData.forEach((pet) => {
     const clone = template.content.cloneNode(true)
+    clone.querySelector(".pet-card").dataset.species = pet.species
     clone.querySelector("h3").textContent = pet.name
     clone.querySelector(".pet-description").textContent = pet.description
     clone.querySelector(".pet-age").textContent = createAgeText(pet.birthYear)
@@ -54,7 +55,19 @@ allButtons.forEach(el => {
 function handleButtonClick(e) {
   //remove active button class from all buttons
   allButtons.forEach(el => el.classList.remove("active"))
+
   //add active class to the button that was clicked
   e.target.classList.add("active")
+
   //actually filter the pets down below
+  const currentFilter = e.target.dataset.filter
+  console.log(currentFilter)
+  document.querySelectorAll(".pet-card").forEach(card => {
+    if (currentFilter == card.dataset.species || currentFilter == "all") {
+      card.style.display = "grid"
+    } else {
+      card.style.display = "none"
+    }
+  })
+
 }
